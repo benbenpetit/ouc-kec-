d9fe3e4bb56b1c1f59eb85943c36a4a8f65e2ccf
Benoit PETIT <petitbenoit3@gmail.com>
2023-12-14 23:59:44 +0100

    refined world cities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
+import CITIES from '@/constants/cities'
+
 const Home = () => {
-  return <main>Oucékecé</main>
+  return (
+    <main>
+      <div className="board">
+        {CITIES.map((city, index) => (
+          <div key={index}>
+            <span>{city.name}</span>
+            <span> | pop: {city.population}</span>
+          </div>
+        ))}
+      </div>
+    </main>
+  )
 }
 
 export default Home
