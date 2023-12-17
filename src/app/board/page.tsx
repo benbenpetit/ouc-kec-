ef45c4d2b7fabd1f442415d11a93a86443054f71
Benoit PETIT <petitbenoit3@gmail.com>
2023-12-18 00:03:13 +0100

    WIP drag cards

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,39 +1,87 @@
+'use client'
+
 import Card from '@/components/Card/Card'
 import CITIES from '@/core/data/constants/cities'
+import { DndContext, PointerSensor, useSensor, useSensors } from '@dnd-kit/core'
+import { SortableContext } from '@dnd-kit/sortable'
+import { useState } from 'react'
+import { v4 as uuid } from 'uuid'
 
 const BoardPage = () => {
-  const startCity = CITIES[0]
-  const citiesSample = CITIES.slice(1, 3)
+  const [round, setRound] = useState(0)
+  const shuffledCities = CITIES.sort(() => 0.5 - Math.random())
+    .slice(0, 46)
+    .map((city) => ({
+      ...city,
+      id: uuid(),
+    }))
+  const deck = [
+    shuffledCities.slice(0, 15),
+    shuffledCities.slice(15, 30),
+    shuffledCities.slice(30, 45),
+  ]
+  const startCity = shuffledCities[45]
+
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 3,
+      },
+    })
+  )
+
+  const onDragStart = () => {
+    console.log('onDragStart')
+  }
+
+  const onDragEnd = () => {
+    console.log('onDragEnd')
+  }
+
+  const onDragOver = () => {
+    console.log('onDragOver')
+  }
 
   return (
     <main>
-      <div className="c-board">
-        <div className="c-board__center">
-          <div className="c-board__center-card">
-            <Card city={startCity} />
-          </div>
-          <div className="c-board__direction --left --row">
-            {citiesSample.map((city, index) => (
-              <Card key={index} city={city} />
-            ))}
-          </div>
-          <div className="c-board__direction --right --row">
-            {citiesSample.map((city, index) => (
-              <Card key={index} city={city} />
-            ))}
-          </div>
-          <div className="c-board__direction --top --column">
-            {citiesSample.map((city, index) => (
-              <Card key={index} city={city} />
-            ))}
-          </div>
-          <div className="c-board__direction --bottom --column">
-            {citiesSample.map((city, index) => (
-              <Card key={index} city={city} />
-            ))}
+      <DndContext
+        sensors={sensors}
+        onDragStart={onDragStart}
+        onDragEnd={onDragEnd}
+        onDragOver={onDragOver}
+      >
+        <div className="c-board">
+          <div className="c-board__center">
+            <div className="c-board__center-card">
+              <Card city={startCity} />
+            </div>
+            {/* <SortableContext items={cardsLeftIds.concat(cardsRightIds)}>
+              <div className="c-board__direction --left">
+                {cardsLeft.map((city, index) => (
+                  <Card key={index} city={city} />
+                ))}
+              </div>
+            </SortableContext>
+            <div className="c-board__direction --right">
+              {cardsRight.map((city, index) => (
+                <Card key={index} city={city} />
+              ))}
+            </div>
+            <SortableContext items={cardsTopIds.concat(cardsBottomIds)}>
+              <div className="c-board__direction --top">
+                {cardsTop.map((city, index) => (
+                  <Card key={index} city={city} />
+                ))}
+              </div>
+              <div className="c-board__direction --bottom">
+                {cardsBottom.map((city, index) => (
+                  <Card key={index} city={city} />
+                ))}
+              </div>
+            </SortableContext> */}
           </div>
         </div>
-      </div>
+      </DndContext>
     </main>
   )
 }
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,6 +4,8 @@ import Back from '@/components/Card/Back'
 import Front from '@/components/Card/Front'
 import { FC, useState } from 'react'
 import { ICity } from '@/core/types/ICity'
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
 
 interface Props {
   city: ICity
@@ -12,8 +14,49 @@ interface Props {
 const Card: FC<Props> = ({ city }) => {
   const [isFlipped, setIsFlipped] = useState(false)
 
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: city.name,
+    data: {
+      type: 'card',
+      city,
+    },
+  })
+
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  }
+
+  if (isDragging) {
+    return (
+      <div
+        className="c-card is-grabbing"
+        ref={setNodeRef}
+        style={style}
+        {...attributes}
+        {...listeners}
+      >
+        <div className="c-card__inside" />
+      </div>
+    )
+  }
+
   return (
-    <div className="c-card" onClick={() => setIsFlipped(!isFlipped)}>
+    <div
+      className="c-card"
+      onClick={() => setIsFlipped(!isFlipped)}
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+    >
       <div className="c-card__inside">
         {!isFlipped ? (
           <Front name={city.name} />
